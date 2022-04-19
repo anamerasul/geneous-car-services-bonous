1ef3754939ee317c67b55f46884758907cae0c6e
anamerasul <anamerasul@gmail.com>
2022-04-19 17:59:56 +0600

    Do not allow protected route without email verification

--- genius-car-service-bonous/src/Pages/Login/RequireAuth/RequireAuth.js
+++ genius-car-service-bonous/src/Pages/Login/RequireAuth/RequireAuth.js
@@ -4,14 +4,34 @@ import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
-const RequireAuth = ({children}) => {
+import { useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { toast, ToastContainer } from 'react-toastify';
+
+const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
+
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation();
-    if(loading){
+    if (loading) {
         return <Loading></Loading>
     }
-    if(!user){
+    if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    if (!user.emailVerified) {
+        return <div>
+
+            <h3 className="text-danger"> your Email is not verified</h3>
+            <button
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('email sent')
+                }}
+            >
+                Verify email
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
     }
     return children;
 };
